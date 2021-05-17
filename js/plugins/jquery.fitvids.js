32778175eacc89e7a43d6a408be853ffe5e41a22
tom-halpin <tom.halpin@dxc.com>
2021-05-17 22:36:18 +0100

    Update jquery.fitvids.js

--- js/plugins/jquery.fitvids.js
+++ js/plugins/jquery.fitvids.js
@@ -5,7 +5,6 @@
 *
 * Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
 * Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
-* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
 *
 */
 
